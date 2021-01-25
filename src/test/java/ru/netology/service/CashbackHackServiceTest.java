7dd06381a3b097e383e470075d238bc388343c33
Inga Oberemchuk <inga_oberemchuk@mail.ru>
2021-01-25 22:46:22 +0300

    Files for task 2 Junit4 added

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,8 +1,8 @@
 package ru.netology.service;
 
-import org.testng.annotations.Test;
+import org.junit.Test;
 
-import static org.testng.Assert.*;
+import static org.junit.Assert.*;
 
 public class CashbackHackServiceTest {
 
@@ -12,27 +12,27 @@ public class CashbackHackServiceTest {
     public void zeroAmount() {
         int actual = cashbackHackService.remain(0);
         int expected = 0;
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
     @Test
     public void boundaryAmount() {
         int actual = cashbackHackService.remain(1000);
         int expected = 0;
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
     @Test
     public void lessThenBoundaryAmount(){
         int actual = cashbackHackService.remain(700);
         int expected = 300;
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
     @Test
     public void moreThenBoundaryAmount(){
         int actual = cashbackHackService.remain(10800);
         int expected = 200;
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 }
